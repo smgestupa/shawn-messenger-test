0b09b8ca82a7c3448ac26d36822e65cf22740228
smgestupa <thereal.nwahs@gmail.com>
2024-03-18 17:31:43 +0800

    switch place of body-parser config

--- src/server.js
+++ src/server.js
@@ -8,15 +8,15 @@ import initWebRoutes from "./routes/web.js";
 const app = express();
 const PORT = process.env.PORT ?? 8080;
 
+// Configure to use `body-parser` to POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Configure view engine
 viewEngine(app);
 
 // Initialize web routes
 initWebRoutes(app);
-
-// Configure to use `body-parser` to POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Application is running at port ${PORT}.`);
