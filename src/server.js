e7fa9a58aa817f2548ef8855328b7c8148e4b3fb
smgestupa <thereal.nwahs@gmail.com>
2024-03-18 17:25:59 +0800

    add body-parser

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 import express from "express";
+import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine.js";
 import initWebRoutes from "./routes/web.js";
 
@@ -12,6 +13,10 @@ viewEngine(app);
 
 // Initialize web routes
 initWebRoutes(app);
+
+// Configure to use `body-parser` to POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Application is running at port ${PORT}.`);
