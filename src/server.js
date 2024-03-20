fc61baf79e2abdc59f1095402eb6ee90366b1b05
smgestupa <thereal.nwahs@gmail.com>
2024-03-21 01:48:12 +0800

    place initializing persistent menu in a self-invoking async function

--- src/server.js
+++ src/server.js
@@ -19,40 +19,42 @@ viewEngine(app);
 initWebRoutes(app);
 
 // Initialize persistent menu
-try {
-    const params = new URLSearchParams({
-        persistent_menu: JSON.stringify([
-            {
-                locale: "default",
-                composer_input_disabled: false,
-                call_to_actions: [
-                    {
-                        type: "postback",
-                        title: "Notify an agent!",
-                        payload: "BTN_TALK_AGENT"
-                    },
-                    {
-                        type: "postback",
-                        title: "Send an inquiry",
-                        payload: "BTN_SEND_INQUIRY"
-                    }
-                ]
-            }
-        ]),
-        whitelisted_domains: JSON.stringify([
-            "https://shawn-messenger-test.onrender.com"
-        ]),
-        access_token: process.env.PAGE_ACCESS_TOKEN
-    });
+(async function() {
+    try {
+        const params = new URLSearchParams({
+            persistent_menu: JSON.stringify([
+                {
+                    locale: "default",
+                    composer_input_disabled: false,
+                    call_to_actions: [
+                        {
+                            type: "postback",
+                            title: "Notify an agent!",
+                            payload: "BTN_TALK_AGENT"
+                        },
+                        {
+                            type: "postback",
+                            title: "Send an inquiry",
+                            payload: "BTN_SEND_INQUIRY"
+                        }
+                    ]
+                }
+            ]),
+            whitelisted_domains: JSON.stringify([
+                "https://shawn-messenger-test.onrender.com"
+            ]),
+            access_token: process.env.PAGE_ACCESS_TOKEN
+        });
+        
+        await fetch(`https://graph.facebook.com/v14.0/me/messenger_profile?${params.toString()}`, {
+            method: "POST"
+        });
     
-    await fetch(`https://graph.facebook.com/v14.0/me/messenger_profile?${params.toString()}`, {
-        method: "POST"
-    });
-
-    console.log("Successfully initialized persistent menu.");
-} catch (error) {
-    console.error(error);
-}
+        console.log("Successfully initialized persistent menu.");
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Application is running at port ${PORT}.`);
